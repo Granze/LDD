9fb9948a014b7a16ed7b6a2780f6cd84f40bbd14
Maurizio Mangione <granzebru@gmail.com>
2018-02-25 21:38:26 +0100

    add push

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '862565107217'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // ...
+});
---
+++ src/notifications.js
@@ -0,0 +1,35 @@
+import firebase from '@firebase/app';
+import '@firebase/messaging';
+
+firebase.initializeApp({
+  'messagingSenderId': '862565107217'
+});
+
+const msgButton = document.querySelector('#notification');
+const messaging = firebase.messaging();
+
+messaging.usePublicVapidKey('BOKq00ci6XQ7aglXfpItWYjG51hG32MRNhI7ztkLktgUS-SsAp8h233YNhwgiu6U9cy1xjkao8eMcs5gT83QyGI');
+
+const subscribe = () => {
+  messaging.requestPermission()
+    .then(() => {
+      console.log('Notification permission granted');
+      return messaging.getToken();
+    })
+    .then(token => {
+      console.log(token);
+    })
+    .catch(err => {
+      console.log('Unable to get permission', err);
+    });
+};
+
+messaging.onMessage(payload => {
+  console.log('on message:', payload);
+});
+
+msgButton.addEventListener('click', subscribe);
+
+
+
+
