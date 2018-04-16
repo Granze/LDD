1b823f9fdb3f3abcfcee2e687bd9f2650cfda7a2
Maurizio Mangione <granzebru@gmail.com>
2018-04-16 23:07:49 +0200

    clean up

--- src/notifications.js
+++ src/notifications.js
@@ -4,28 +4,12 @@ import '@firebase/messaging';
 firebase.initializeApp({
   'messagingSenderId': '862565107217'
 });
-
-const msgButton = document.querySelector('#notification');
 const messaging = firebase.messaging();
-
-messaging.usePublicVapidKey('BOKq00ci6XQ7aglXfpItWYjG51hG32MRNhI7ztkLktgUS-SsAp8h233YNhwgiu6U9cy1xjkao8eMcs5gT83QyGI');
+messaging.usePublicVapidKey('BOKq00ci6XQ7aglXfpItWYjG...');
 
 messaging.requestPermission()
-  .then(() => {
-    return messaging.getToken();
-  })
-  .then(token => {
-    console.log(token);
-  })
-  .catch(err => {
-    console.log('Unable to get permission', err);
-  });
+  .then(() => messaging.getToken())
+  .then(token => console.log(token))
+  .catch(err => console.log('Unable to get permission', err);
 
-messaging.onMessage(payload => {
-  console.log('on message:', payload);
-});
-
-
-
-
-
+messaging.onMessage(payload => console.log('on message:', payload));
