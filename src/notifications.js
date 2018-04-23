de33097289865166d3df8e0df1126c8142a1afb7
Maurizio Mangione <granzebru@gmail.com>
2018-04-23 16:03:07 +0200

    minor

--- src/notifications.js
+++ src/notifications.js
@@ -10,6 +10,6 @@ messaging.usePublicVapidKey('BOKq00ci6XQ7aglXfpItWYjG...');
 messaging.requestPermission()
   .then(() => messaging.getToken())
   .then(token => console.log(token))
-  .catch(err => console.log('Unable to get permission', err);
+  .catch(err => console.log('Unable to get permission', err)
 
 messaging.onMessage(payload => console.log('on message:', payload));
